9b935a911a8836b2ef30c8664d2d814067a02ad9
09decdev <hungvyn1@gmail.com>
2025-05-19 14:05:28 +0700

    first commit

---
+++ src/main/java/oct/example/oct_vy_tai/OctVyTaiApplication.java
@@ -0,0 +1,13 @@
+package oct.example.oct_vy_tai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctVyTaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctVyTaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oct/example/oct_vy_tai/OctVyTaiApplicationTests.java
@@ -0,0 +1,13 @@
+package oct.example.oct_vy_tai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctVyTaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
